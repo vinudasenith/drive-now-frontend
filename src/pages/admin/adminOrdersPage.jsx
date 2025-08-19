675be6f039be1f0ce32d450538dfabebc3bf4242
vinudasenith <ha.vinudas@gmail.com>
2025-08-19 08:44:06 +0530

    fix issue in naming✔️

--- src/pages/admin/adminOrdersPage.jsx
+++ src/pages/admin/adminOrdersPage.jsx
@@ -61,7 +61,7 @@ export default function AdminOrdersPage() {
 
     return (
         <div className="p-6">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">Admin Orders</h1>
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">Admin Bookings</h1>
             {loading ? (
                 <p className="text-center text-gray-500 italic">Loading...</p>
             ) : (
--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -36,7 +36,7 @@ export default function AdminPage() {
                         className="flex items-center gap-3 text-lg font-medium hover:bg-gray-800 px-4 py-2 rounded transition"
                     >
                         <BsFillSaveFill className="text-yellow-400" />
-                        Orders
+                        Bookings
                     </Link>
 
                     <Link
